a24455c7f845ddcb2c3c0049da28bb885d1188b2
umeshmahato28 <mahatoumesh94@gmail.com>
2024-08-23 13:07:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express()
+
+app.all('/', (req, res) =>{
+      console.log('Request >', req);
+      console.log('Response >', res);
+      res.send(`I'm up!`)
+      
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {
+      console.log(`Server is running at port ${PORT}`);
+      
+})
